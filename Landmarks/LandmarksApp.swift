68856bf8b1d990bb28ae8a628a0f39633be9fea0
ridhoafwani <zikrurridhoafwani@gmail.com>
2024-03-02 10:19:13 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Muhammad Zikrurridho Afwani on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
